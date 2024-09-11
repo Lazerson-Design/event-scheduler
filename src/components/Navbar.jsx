db2c013d4a73e5b9c32db8fa5cf7f637ab6dccb9
ioan ionac <ionaci@ioans-air.localdomain>
2024-09-11 08:19:05 +0200

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,25 @@ const Navbar = () => {
         <a className="btn btn-ghost text-xl">Event-Scheduler</a>
       </div>
       <div className="flex-none ">
-        <ul className="menu menu-horizontal px-1 gap-5 font-bold ">
+        <ul className="menu menu-horizontal px-1 gap-5 font-bold mr-10">
           <li>
             <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <NavLink to="/SignUp">Sign up</NavLink>
+            <NavLink to="/addEvent">Add Event</NavLink>
           </li>
-          <li>
-            <NavLink to="/SignIn">Sign in</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Event">Event</NavLink>
-          </li>
-          <li>
-            <NavLink to="/EventDetails">Event details</NavLink>
-          </li>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img
+                alt="Tailwind CSS Navbar component"
+                src="https://imgs.search.brave.com/twkipH7hdreu1BLGIZl4bXwBFPxGBtbLmKzy8xeplVI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTMw/MzI3NDI3NS92ZWN0/b3IvZC1zdHlsZS1s/b2dvLWljb24tc2hh/cGUuanBnP3M9NjEy/eDYxMiZ3PTAmaz0y/MCZjPVEweTNURERC/cnJGQndSQ2R6VGdf/T1hMaGhwUGdoMFIt/YzctLVZqYmJTSnM9"
+              />
+            </div>
+          </div>
         </ul>
       </div>
     </div>
